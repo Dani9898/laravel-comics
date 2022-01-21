4cc1201e6f5240ad8e88262bf0b4d105cd42290a
Dani9898 <danieleoliva98@gmail.com>
2022-01-21 16:29:49 +0100

    feat: home-content

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,144 @@
 
 @section('content')
 
+<div id="container">
 
+    <div id="current-series">CURRENT SERIES</div>
+
+    <div class="serie">
+
+        <div class="serie-img">
+            <img src="https://image.api.playstation.com/vulcan/img/rnd/202010/2122/19xis9VT5XfnXsLSMkxUjvVd.png?w=440">
+        </div>
+
+        <div class="serie-name">
+            <span>Action Comics</span>
+        </div>
+    
+    </div>
+    <div class="serie">
+        
+        <div class="serie-img">
+            <img src="https://image.api.playstation.com/vulcan/img/rnd/202010/2122/19xis9VT5XfnXsLSMkxUjvVd.png?w=440">
+        </div>
+
+        <div class="serie-name">
+            <span>Action Comics</span>
+        </div>
+    
+    </div>
+    <div class="serie">
+        
+        <div class="serie-img">
+            <img src="https://image.api.playstation.com/vulcan/img/rnd/202010/2122/19xis9VT5XfnXsLSMkxUjvVd.png?w=440">
+        </div>
+
+        <div class="serie-name">
+            <span>Action Comics</span>
+        </div>
+    
+    </div>
+    <div class="serie">
+        
+        <div class="serie-img">
+            <img src="https://image.api.playstation.com/vulcan/img/rnd/202010/2122/19xis9VT5XfnXsLSMkxUjvVd.png?w=440">
+        </div>
+
+        <div class="serie-name">
+            <span>Action Comics</span>
+        </div>
+    
+    </div>
+    <div class="serie">
+        
+        <div class="serie-img">
+            <img src="https://image.api.playstation.com/vulcan/img/rnd/202010/2122/19xis9VT5XfnXsLSMkxUjvVd.png?w=440">
+        </div>
+
+        <div class="serie-name">
+            <span>Action Comics</span>
+        </div>
+    
+    </div>
+    <div class="serie">
+        
+        <div class="serie-img">
+            <img src="https://image.api.playstation.com/vulcan/img/rnd/202010/2122/19xis9VT5XfnXsLSMkxUjvVd.png?w=440">
+        </div>
+
+        <div class="serie-name">
+            <span>Action Comics</span>
+        </div>
+    
+    </div>
+    <div class="serie">
+        
+        <div class="serie-img">
+            <img src="https://image.api.playstation.com/vulcan/img/rnd/202010/2122/19xis9VT5XfnXsLSMkxUjvVd.png?w=440">
+        </div>
+
+        <div class="serie-name">
+            <span>Action Comics</span>
+        </div>
+    
+    </div>
+    <div class="serie">
+        
+        <div class="serie-img">
+            <img src="https://image.api.playstation.com/vulcan/img/rnd/202010/2122/19xis9VT5XfnXsLSMkxUjvVd.png?w=440">
+        </div>
+
+        <div class="serie-name">
+            <span>Action Comics</span>
+        </div>
+    
+    </div>
+    <div class="serie">
+        
+        <div class="serie-img">
+            <img src="https://image.api.playstation.com/vulcan/img/rnd/202010/2122/19xis9VT5XfnXsLSMkxUjvVd.png?w=440">
+        </div>
+
+        <div class="serie-name">
+            <span>Action Comics</span>
+        </div>
+    
+    </div>
+    <div class="serie">
+        
+        <div class="serie-img">
+            <img src="https://image.api.playstation.com/vulcan/img/rnd/202010/2122/19xis9VT5XfnXsLSMkxUjvVd.png?w=440">
+        </div>
+
+        <div class="serie-name">
+            <span>Action Comics</span>
+        </div>
+    
+    </div>
+    <div class="serie">
+        
+        <div class="serie-img">
+            <img src="https://image.api.playstation.com/vulcan/img/rnd/202010/2122/19xis9VT5XfnXsLSMkxUjvVd.png?w=440">
+        </div>
+
+        <div class="serie-name">
+            <span>Action Comics</span>
+        </div>
+    
+    </div>
+    <div class="serie">
+        
+        <div class="serie-img">
+            <img src="https://image.api.playstation.com/vulcan/img/rnd/202010/2122/19xis9VT5XfnXsLSMkxUjvVd.png?w=440">
+        </div>
+
+        <div class="serie-name">
+            <span>Action Comics</span>
+        </div>
+    
+    </div>
+
+    <div id="btn-more">LOAD MORE</div>
+</div>
 
 @endsection
