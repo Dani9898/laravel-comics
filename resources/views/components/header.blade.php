5f933a251ae7ad47788c4f498606ee8ad3016b84
Dani9898 <danieleoliva98@gmail.com>
2022-01-21 15:52:27 +0100

    feat: footer

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,18 @@
+<header>
+    {{-- <img src="../assets/img/dc-logo.png" alt=""> --}}
+      {{-- <nav>
+        <ul>
+          <li class= 'active' ><a href="#">CHARACTERS</a></li>
+          <li class= 'active' ><a href="#">COMICS</a></li>
+          <li class= 'active' ><a href="#">MOVIES</a></li>
+          <li class= 'active' ><a href="#">TV</a></li>
+          <li class= 'active' ><a href="#">GAMES</a></li>
+          <li class= 'active' ><a href="#">COLLECTIBLES</a></li>
+          <li class= 'active' ><a href="#">VIDEOS</a></li>
+          <li class= 'active' ><a href="#">FANS</a></li>
+          <li class= 'active' ><a href="#">NEWS</a></li>
+          <li class= 'active' ><a href="#">SHOP</a></li>
+        </ul>
+      </nav> --}}
+      <h1>questo è l'header</h1>
+</header>
