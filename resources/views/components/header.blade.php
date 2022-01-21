75dce5b898bb21bcadfade8e1b20f53d2de7d12d
Dani9898 <danieleoliva98@gmail.com>
2022-01-21 16:03:54 +0100

    feat: header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,18 +1,18 @@
 <header>
-    {{-- <img src="../assets/img/dc-logo.png" alt=""> --}}
-      {{-- <nav>
+    <img  style = "width: 100px" src="https://images.unsplash.com/photo-1494253109108-2e30c049369b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8cmFuZG9tfGVufDB8fDB8fA%3D%3D&w=1000&q=80">
+      <nav>
         <ul>
-          <li class= 'active' ><a href="#">CHARACTERS</a></li>
+          <li><a href="#">CHARACTERS</a></li>
           <li class= 'active' ><a href="#">COMICS</a></li>
-          <li class= 'active' ><a href="#">MOVIES</a></li>
-          <li class= 'active' ><a href="#">TV</a></li>
-          <li class= 'active' ><a href="#">GAMES</a></li>
-          <li class= 'active' ><a href="#">COLLECTIBLES</a></li>
-          <li class= 'active' ><a href="#">VIDEOS</a></li>
-          <li class= 'active' ><a href="#">FANS</a></li>
-          <li class= 'active' ><a href="#">NEWS</a></li>
-          <li class= 'active' ><a href="#">SHOP</a></li>
+          <li><a href="#">MOVIES</a></li>
+          <li><a href="#">TV</a></li>
+          <li><a href="#">GAMES</a></li>
+          <li><a href="#">COLLECTIBLES</a></li>
+          <li><a href="#">VIDEOS</a></li>
+          <li><a href="#">FANS</a></li>
+          <li><a href="#">NEWS</a></li>
+          <li><a href="#">SHOP</a></li>
         </ul>
-      </nav> --}}
-      <h1>questo è l'header</h1>
+      </nav>
+
 </header>
