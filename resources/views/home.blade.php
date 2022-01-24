2fa98d430c842e65f01e9995d60d1a232871ce00
Dani9898 <danieleoliva98@gmail.com>
2022-01-24 13:29:53 +0100

    aggiunto link a comic a tutti i fumetti

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -20,9 +20,11 @@
         </div>
 
         <div class="serie">
-            <div class="serie-img">
-                <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/AV1976_01_300-001_HD_5f738f6e39ddd7.18205602.jpg?itok=VgdYdJ01">
-            </div>
+            <a href="{{ route('comic') }}">
+                <div class="serie-img">
+                    <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/AV1976_01_300-001_HD_5f738f6e39ddd7.18205602.jpg?itok=VgdYdJ01">
+                </div>
+            </a>
 
             <div class="serie-name">
                 <span>Action Comics</span>
@@ -30,9 +32,11 @@
         </div>
 
     <div class="serie">
-        <div class="serie-img">
-            <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2018/08/AQMv4_UW_153-001_HD_5b7efa1745a9b8.08589135.jpg?itok=HPTtnrsb">
-        </div>
+        <a href="{{ route('comic') }}">
+            <div class="serie-img">
+                <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2018/08/AQMv4_UW_153-001_HD_5b7efa1745a9b8.08589135.jpg?itok=HPTtnrsb">
+            </div>
+        </a>
 
         <div class="serie-name">
             <span>Action Comics</span>
@@ -40,9 +44,11 @@
     </div>
 
     <div class="serie">
-        <div class="serie-img">
-            <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/06/batgirl_v5_1_5b23e1f1124941.50950783.jpg?itok=VnrJah76">
-        </div>
+        <a href="{{ route('comic') }}">
+            <div class="serie-img">
+                <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/06/batgirl_v5_1_5b23e1f1124941.50950783.jpg?itok=VnrJah76">
+            </div>
+        </a>
 
         <div class="serie-name">
             <span>Action Comics</span>
@@ -50,9 +56,11 @@
     </div>
 
     <div class="serie">
-        <div class="serie-img">
-            <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/BM_56_300-001_HD_5ba137a85c3bf3.33122291.jpg?itok=3FHJQYJZ">
-        </div>
+        <a href="{{ route('comic') }}">
+            <div class="serie-img">
+                <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/BM_56_300-001_HD_5ba137a85c3bf3.33122291.jpg?itok=3FHJQYJZ">
+            </div>
+        </a>
 
         <div class="serie-name">
             <span>Action Comics</span>
@@ -60,9 +68,11 @@
     </div>
 
     <div class="serie">      
-        <div class="serie-img">
-            <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/BM_56_300-001_HD_5ba137a85c3bf3.33122291.jpg?itok=3FHJQYJZ">
-        </div>
+        <a href="{{ route('comic') }}">
+            <div class="serie-img">
+                <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/BM_56_300-001_HD_5ba137a85c3bf3.33122291.jpg?itok=3FHJQYJZ">
+            </div>
+        </a>
 
         <div class="serie-name">
             <span>Action Comics</span>
@@ -70,9 +80,11 @@
     </div>
 
     <div class="serie">    
-        <div class="serie-img">
-            <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2019/08/BMSM_5d4361b7116261.74371456.jpg?itok=kEDR2OL8">
-        </div>
+        <a href="{{ route('comic') }}">
+            <div class="serie-img">
+                <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2019/08/BMSM_5d4361b7116261.74371456.jpg?itok=kEDR2OL8">
+            </div>
+        </a>
 
         <div class="serie-name">
             <span>Action Comics</span>
@@ -80,9 +92,11 @@
     </div>
 
     <div class="serie"> 
-        <div class="serie-img">
-            <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/BMSMANN_01_300-001_HD_5f5ff17fa1d665.74704970.jpg?itok=azz5sfGk">
-        </div>
+        <a href="{{ route('comic') }}">
+            <div class="serie-img">
+                <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/BMSMANN_01_300-001_HD_5f5ff17fa1d665.74704970.jpg?itok=azz5sfGk">
+            </div>
+        </a>
 
         <div class="serie-name">
             <span>Action Comics</span>
@@ -90,9 +104,11 @@
     </div>
 
     <div class="serie">
-        <div class="serie-img">
-            <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/BMTJWZ_01_300-001_HD_5f5ff2307dcb37.34652945.jpg?itok=VswVjLR8">
-        </div>
+        <a href="{{ route('comic') }}">
+            <div class="serie-img">
+                <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/BMTJWZ_01_300-001_HD_5f5ff2307dcb37.34652945.jpg?itok=VswVjLR8">
+            </div>
+        </a>
 
         <div class="serie-name">
             <span>Action Comics</span>
@@ -100,9 +116,11 @@
     </div>
 
     <div class="serie">       
-        <div class="serie-img">
-            <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/08/cover-v2_5f40314645a734.37285065.png?itok=ImNM2QBY">
-        </div>
+        <a href="{{ route('comic') }}">
+            <div class="serie-img">
+                <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/08/cover-v2_5f40314645a734.37285065.png?itok=ImNM2QBY">
+            </div>
+        </a>
 
         <div class="serie-name">
             <span>Action Comics</span>
@@ -110,18 +128,22 @@
     </div>
 
     <div class="serie">
-        <div class="serie-img">
-            <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/10/BMWK_HQ_01_300-001_HD_5f7cb4945e13f6.89501032.jpg?itok=sVwALbUX">
-        </div>
+        <a href="{{ route('comic') }}">
+            <div class="serie-img">
+                <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/10/BMWK_HQ_01_300-001_HD_5f7cb4945e13f6.89501032.jpg?itok=sVwALbUX">
+            </div>
+        </a>
         <div class="serie-name">
             <span>Action Comics</span>
         </div>
     </div>
 
     <div class="serie"> 
-        <div class="serie-img">
-            <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2019/04/CTWv1_CC_144-001_HD_5ca5299a751963.53054221.jpg?itok=ooPaoLDq">
-        </div>
+        <a href="{{ route('comic') }}">
+            <div class="serie-img">
+                <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2019/04/CTWv1_CC_144-001_HD_5ca5299a751963.53054221.jpg?itok=ooPaoLDq">
+            </div>
+        </a>
 
         <div class="serie-name">
             <span>Action Comics</span>
